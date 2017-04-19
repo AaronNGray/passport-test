8a650bd54e0462910381bd31588a501b3c0ecd0f
AaronNGray <aaronngray@gmail.com>
2017-04-19 01:13:21 +0100

    Added extra 'service' and 'profile' fields to 'models/user.js' to store profile and service vendors to MongoDB

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 // create a user model
 var User = mongoose.model('User', {
   oauthID: String,
+  service: String,
+  profile: Object,
   name: String,
   created: Date
 });
